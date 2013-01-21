7a4b817ca2fc728895328fd845e694e4efe775ee
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-01-21 09:17:22 -0500

    Create AiEntity class working on: - collision detection (some weird things when colliding w/hero) - moving withing domain (doesn't seem to work) - doesn't seem to spawn at spawnPoint - randomMove doesn't run infinitely - need to trigger collision on hitting world bounds

--- Game/Game.passiveObjects.js
+++ Game/Game.passiveObjects.js
@@ -44,9 +44,46 @@ Game.vent.on("game:initialized", function() {
 		hitRadius: 30
 	})
 
-	createjs.Utility.randomSpawn(Game.Pipe, Game.world, 10);
-	createjs.Utility.randomSpawn(Game.Hill, Game.world, 5);
-	createjs.Utility.randomSpawn(Game.Bouquet, Game.world, 10);
+	Game.Koopa = createjs.AiEntity.extend({
+		world: Game.world,
+
+		movements: [
+			{ direction: "down",			frames: [0, 1, 0, 2] },
+			{ direction: "downRight", frames: [3, 4, 3, 5] },
+			{ direction: "right",			frames: [6, 7, 6, 8] },
+			{ direction: "upRight", 	frames: [9, 10, 9, 11] },
+			{ direction: "up", 				frames: [12, 13, 12, 14] },
+			{ direction: "upLeft", 	frames: [15, 16, 15, 17] },
+			{ direction: "left",			frames: [18, 19, 18, 20] },
+			{ direction: "downLeft", 		frames: [21, 22, 21, 23] },
+		],
+
+		spriteOptions: {
+			images: [Game.images.mario_movements],
+			frames: { width: 100, height: 100, regX: 50, regY: 85 },
+			frequency: 3
+		},
+
+		hitRadius:15
+	});
+
+	window.koopa = new Game.Koopa({
+		spawnPoint: {
+			x: 300, y: 300
+		},
+		domain: {
+			x: 300, y: 300,
+			width: 300, height: 300
+		}
+	})
+
+	Game.world.addEntity(koopa);
+
+
+
+	createjs.Utility.randomSpawn(Game.Pipe, Game.world, 5);
+	createjs.Utility.randomSpawn(Game.Hill, Game.world, 3);
+	createjs.Utility.randomSpawn(Game.Bouquet, Game.world, 5);
 
 	/*window.pipe = new Game.Pipe({
 		spawnPoint: {
